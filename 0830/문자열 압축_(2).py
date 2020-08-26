6c8948e1ec00b6676ac34180fd0ad53c550b3170
gaberani <khs0783@naver.com>
2020-08-26 23:58:19 +0900

    프로그래머스 문자열 압축

--- 0830/문자열 압축_(2).py
+++ 0830/문자열 압축_(2).py
@@ -1,7 +1,31 @@
 def solution(s):
     # 표현한 문자열 중 가장 짧은 것의 길이 (answer)
     answer = 0
-    for L in range(1, len(s)//2):
+    L = len(s)
+    if L == 1:
+        return L
+    answer = L
+    for l in range(1, L // 2 + 1):  # 자를 단위(절반까지 가능)
+        cut_answer = ''
+        word = s[:l]
+        cnt = 1
+        for sl in range(l, L, l):  # 단위만큼만 비교
+            if word == s[sl:sl + l]:
+                cnt += 1
+            else:
+                if cnt == 1:
+                    cut_answer += word
+                else:
+                    cut_answer += str(cnt) + word
+                    cnt = 1  # cnt 초기화
+                word = s[sl:sl + l]
 
+        # 끝난 뒤 마지막 부분에 따라 추가
+        if cnt == 1:
+            cut_answer += word
+        else:
+            cut_answer += str(cnt) + word
 
+        if answer > len(cut_answer):
+            answer = len(cut_answer)
     return answer
