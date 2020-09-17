b9ac01624e1c78c10435165c7174752d64fcd9f4
gaberani <khs0783@naver.com>
2020-09-17 23:37:53 +0900

    프로그래머스 경주로 건설

--- 0913/경주로 건설.py
+++ 0913/경주로 건설.py
@@ -1,3 +1,39 @@
+di = [0, 1,  0, -1]
+dj = [1, 0, -1, 0]
+# import pprint
+
+def dfs(si, sj, cost, board, N, visit, pre):
+    global answer
+    # 도착 했으면
+    if si == N-1 and sj == N-1:
+        if answer > cost:
+            answer = cost
+            # pprint.pprint(visit)
+            print(cost)
+    # 이 부분을 빼고 visit에 최소 금액을 표시하면서 만들기
+    # 아직 도착 안했으면
+    # elif answer < cost:
+    #     return
+    else:
+        for d in range(4):
+            ni, nj = si+di[d], sj+dj[d]
+            if 0 <= ni < N and 0 <= nj < N and board[ni][nj] == 0 and visit[ni][nj] == 0:
+                # 직선 도로
+                if not cost == 0:
+                    if pre%2 == d%2:
+                        visit[ni][nj] = 1
+                        dfs(ni, nj, cost+100, board, N, visit, d)
+                        visit[ni][nj] = 0
+                    # 곡선 도로
+                    else:
+                        visit[ni][nj] = 1
+                        dfs(ni, nj, cost+600, board, N, visit, d)
+                        visit[ni][nj] = 0
+                else:
+                    visit[ni][nj] = 1
+                    dfs(ni, nj, cost + 100, board, N, visit, d)
+                    visit[ni][nj] = 0
+
 def solution():
     answer = 0
     return answer
