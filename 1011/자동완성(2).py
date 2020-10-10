114f4450d8a0a5ddb0d1c9eea26ee403cecc406f
gaberani <khs0783@naver.com>
2020-10-10 17:14:23 +0900

    프로그래머스 자동완성

--- 1011/자동완성(2).py
+++ 1011/자동완성(2).py
@@ -1,23 +1,65 @@
+class Trie():
+    def __init__(self):
+        self.next = dict()
+        self.value = 0
+
 def solution(words):
-    return
+    answer = 0
+    tree = Trie()
+    for word in words:
+        subtree = tree
+        for idx, val in enumerate(word):
+            print(idx, val)
+            subtree.value += 1
+            # 문자가 서브트리의 딕셔너리에 없으면 만듬
+            if val not in subtree.next:
+                subtree.next[val] = Trie()
+            print(subtree.next)
+            subtree = subtree.next[val]
+            if (idx == len(word) - 1):
+                subtree.value += 1
 
+    for word in words:
+        subtree = tree
+        counts = 0
+        for idx, val in enumerate(word):
+            if (subtree.value == 1):
+                answer += counts
+                break
+            elif idx == len(word) - 1:
+                answer += counts + 1
+                break
+            else:
+                subtree = subtree.next[val]
+                counts += 1
 
+    return answer
+
+print(solution(["go", "gone", "guild"]))                # 7
+print(solution(["abc", "def", "ghi", "jklm"]))          # 4
+print(solution(["word", "war", "warrior", "world"]))    # 15
 
 # def solution(words):
-#     words.sort() # 학습된 단어이므로 정렬해도 무방
+#     words.sort()
 #     n, answer = len(words), 0
 #     arr = [0]*n
 #     for i in range(n-1):
 #         cnt1, cnt2 = find(words[i], words[i+1])
+#         # 최대값으로 갱신
 #         arr[i], arr[i+1] = max(arr[i], cnt1), max(arr[i+1], cnt2)
 #     return sum(arr)
 #
 # def find(s1, s2): # 두 개의 글자를 비교하는 함수
-#     cnt, i = 0, 0
+#     pre, cur = 0, 0
 #     while 1:
-#         cnt += 1
-#         if i == s1.__len__(): return cnt-1, cnt # 한 쪽 길이가 짧고, 그때까지 단어가 모두 같을 때
-#         elif i == s2.__len__(): return cnt, cnt-1
-#         else:
-#             if s1[i] != s2[i]: return cnt, cnt # 다른 문자를 가질 때
-#         i += 1
+#         pre += 1
+#         # s1의 길이와 같아질 경우
+#         if cur == len(s1):
+#             return pre-1, pre
+#         # s2의 길이와 같아질 경우
+#         elif cur == len(s2):
+#             return pre, pre-1
+#         # 글자가 달라짐.
+#         elif s1[cur] != s2[cur]:
+#             return pre, pre # 다른 문자를 가질 때
+#         cur += 1
