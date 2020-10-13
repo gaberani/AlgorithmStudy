c3cc4fe9cc160d5fc5f47971abf85e80763656be
gaberani <khs0783@naver.com>
2020-10-13 19:49:34 +0900

    프로1013 알고스íë리즘 스터디

--- 1013/최적의 행렬 곱셈.py
+++ 1013/최적의 행렬 곱셈.py
@@ -15,10 +15,53 @@
 # 행렬의 개수는 3이상 200이하의 자연수입니다.
 # 각 행렬의 행과 열의 크기는 200이하의 자연수 입니다.
 # 계산을 할 수 없는 행렬은 입력으로 주어지지 않습니다.
+import sys
 def solution(matrix_sizes):
     # 모든 행렬을 곱하기 위한 최소 곱셈 연산의 수를 return
-    answer = 0
+    answer = 200**3
     # 큰 수를 가운데에 놔서 연산 수를 작게 만드는 것이 핵심
     # 행렬이 4개라면 AB, CD로 연산한뒤 그 둘의 결과를 연산하는 것도
     # 가능해야함
+
+    def dfs(cnt, i, visit, result):
+        if cnt == i:
+            return result
+        else:
+            for j in range(L-1):
+                if visit[j] == 0:
+                    if matrix_sizes[j][1] == matrix_sizes[j+1][0]:
+                        visit[j], visit[j+1] = 1, 1
+                        dfs(cnt+1, L, visit, result+matrix_sizes[j][0]*matrix_sizes[j][1]*matrix_sizes[j+1][1])
+                        visit[j], visit[j+1] = 0, 0
+
+                    if matrix_sizes[j+1][1] == matrix_sizes[j][0]:
+                        visit[j], visit[j + 1] = 1, 1
+                        dfs(cnt+1, L, visit, result+matrix_sizes[j+1][0]*matrix_sizes[j+1][1]*matrix_sizes[j][1])
+                        visit[j], visit[j + 1] = 0, 0
+
+    L = len(matrix_sizes)
+    answer = min(dfs(0, L-1, [0]*L, 0), answer)
+
     return answer
+
+
+
+
+
+# def sol(cnt, N, visit, mat, Max):
+#     if cnt == N:
+#         return 0
+#     if str(cnt)+str(N) in visit:
+#         return visit[str(cnt) + str(N)]
+#     result = Max
+#     for a in range(cnt, N):
+#         result = min(result, sol(cnt, a, visit, mat, Max) + sol(a + 1, N, visit, mat, Max) + mat[cnt][0] * mat[a][1] * mat[N][1])
+#     visit[str(cnt) + str(N)] = result
+#     return result
+#
+# def solution(matrix_sizes):
+#     mat = matrix_sizes
+#     visit = {}
+#     N = len(matrix_sizes)
+#     answer = sol(0, N-1, visit, matrix_sizes, 200*200*200*N)
+#     return answer
