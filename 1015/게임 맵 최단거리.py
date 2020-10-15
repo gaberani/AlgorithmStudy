d24de77397f4ea30e45b0dc0b39fec2e9e9189b6
gaberani <khs0783@naver.com>
2020-10-15 19:01:01 +0900

    프로그래머스 게임 맵 최단거리 완료

--- 1015/게임 맵 최단거리.py
+++ 1015/게임 맵 최단거리.py
@@ -7,6 +7,54 @@
 # 당신의 캐릭터가 (행: 1, 열: 1) 위치에 있고,
 # 상대 팀 진영은 (행: 5, 열: 5) 위치에 있는 경우의 예시입니다.
 
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
 def solution(maps):
-    answer = 0
+    N, M = len(maps), len(maps[0])
+    visit = [[0]*M for _ in range(N)]
+    visit[0][0] = 1
+    s = []
+    bi, bj = 0, 0
+    s.append((bi, bj, 1))
+    while 1:
+        tmp = []
+        while s:
+            bi, bj, cnt = s.pop()
+            for d in range(4):
+                ni, nj = bi+di[d], bj+dj[d]
+                if 0 <= ni < N and 0 <= nj < M and visit[ni][nj] == 0 and maps[ni][nj] == 1:
+                    visit[ni][nj] = cnt+1
+                    tmp.append((ni, nj, cnt+1))
+                if ni == N-1 and nj == M-1:
+                    bi, bj = N-1, M-1
+                    s, tmp = [], []
+                    break
+            # for v in visit: print(v)
+            # print('----------------')
+
+        if tmp:
+            s, tmp = tmp, []
+        else:
+            break
+
+    if bi == N-1 and bj == M-1:
+        answer = cnt + 1
+    else:
+        answer = -1
+
     return answer
+
+
+print(solution([[1,0,1,1,1],
+                [1,0,1,0,1],
+                [1,0,1,1,1],
+                [1,1,1,0,1],
+                [0,0,0,0,1]]))
+
+print(solution([[1,0,1,1,1],
+                [1,0,1,0,1],
+                [1,0,1,1,1],
+                [1,1,1,0,0],
+                [0,0,0,0,1]]))
+
+print(solution([[1, 0]]))
