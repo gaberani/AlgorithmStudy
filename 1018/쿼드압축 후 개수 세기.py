47f8266b990728123e8bffdd471acbabda9d8cf6
gaberani <59900375+gaberani@users.noreply.github.com>
2020-10-17 23:51:29 +0900

    Update 쿼드압축 후 개수 세기.py
    프로그래머스 쿼드압축 후 개수 세기

--- 1018/쿼드압축 후 개수 세기.py
+++ 1018/쿼드압축 후 개수 세기.py
@@ -2,10 +2,12 @@ def solution(arr):
     answer = []
     L = len(arr)
     N = 0
+    # 행렬의 길이가 2의 몇 제곱인지 구하기
     while L > 1:
         L //= 2
         N += 1
 
+    # 시작점 설정을 어떻게 해야할지 고민이 더 필요하다
     for binary in range(N):
         start = arr[N-binary][N-binary]
         for i in range(binary**2):
