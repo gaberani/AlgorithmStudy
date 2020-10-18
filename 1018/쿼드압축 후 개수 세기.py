a451a4bdf69b01d84ca07807040861ea798c51f8
gaberani <khs0783@naver.com>
2020-10-18 09:48:19 +0900

    프로그래머스 쿼드압축 후 개수 세기 완료

--- 1018/쿼드압축 후 개수 세기.py
+++ 1018/쿼드압축 후 개수 세기.py
@@ -6,10 +6,23 @@ def solution(arr):
         L //= 2
         N += 1
 
-    for binary in range(N):
-        start = arr[N-binary][N-binary]
-        for i in range(binary**2):
-            for j in range(binary**2):
-                pass
+    tmp = []
+    def dfs(si, sj, cnt):
+        start = arr[si][sj]
+        for i in range(si, si+2**cnt):
+            for j in range(sj, sj+2**cnt):
+                if arr[i][j] != start:
+                    dfs(si, sj, cnt-1)
+                    dfs(si, sj+2**(cnt-1), cnt-1)
+                    dfs(si+2**(cnt-1), sj, cnt-1)
+                    dfs(si+2**(cnt-1), sj+2**(cnt-1), cnt-1)
+                    return
+        # 전부 시작값과 같으면 숫자 하나로 압축
+        tmp.append(start)
 
+    dfs(0, 0, N)
+    answer = [tmp.count(0), tmp.count(1)]
     return answer
+
+print(solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]))
+print(solution([[1,1,1,1,1,1,1,1],[0,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1],[0,1,0,0,1,1,1,1],[0,0,0,0,0,0,1,1],[0,0,0,0,0,0,0,1],[0,0,0,0,1,0,0,1],[0,0,0,0,1,1,1,1]]))
