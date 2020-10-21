98b3c9a889f33f506bc1e2de7c575d04cb8bb43d
gaberani <khs0783@naver.com>
2020-10-21 23:38:09 +0900

    프로그래머스 야근 지수 등록

---
+++ 1022/야근 지수.py
@@ -0,0 +1,14 @@
+# 야근 피로도는 야근을 시작한 시점에서 남은 일의 작업량을 제곱하여 더한 값
+# Demi는 N시간 동안 야근 피로도를 최소화하도록 일할 겁니다.
+# Demi가 1시간 동안 작업량 1만큼을 처리할 수 있다고 할 때,
+
+# 퇴근까지 남은 N 시간과 각 일에 대한 작업량 works에 대해
+# 야근 피로도를 최소화한 값을 리턴하는 함수 solution
+def solution(n, works):
+    answer = 0
+    return answer
+
+
+print(solution([4, 3, 3], 4))
+print(solution([2, 1, 2], 1))
+print(solution([1, 1], 3))
