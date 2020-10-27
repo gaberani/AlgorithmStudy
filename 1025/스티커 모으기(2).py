3641f2ce21d5ebc60b8978c0170dc522dc389f19
gaberani <khs0783@naver.com>
2020-10-27 19:13:01 +0900

    10월 27일 스터디

--- 1025/스티커 모으기(2).py
+++ 1025/스티커 모으기(2).py
@@ -2,41 +2,27 @@
 # 뜯어낸 스티커에 적힌 숫자의 합이 최대가 되도록 하고 싶습니다.
 # 단, 스티커 한 장을 뜯어내면 양쪽으로 인접해있는 스티커는 찢어져서 사용할 수 없게 됩니다.
 
-# 2.
-def solution3(sticker):
-    answer = 0
+# DP
+def solution(sticker):
     L = len(sticker)
-    visit = [0]*L
+    if L < 4:
+        return max(sticker)
+    front = [0]*L
+    front[0], front[1] = sticker[0], sticker[0]
+    for i in range(2, L-1):
+        front[i] = max(front[i-1], front[i-2]+sticker[i])
 
-    def dfs(tmp):
-        nonlocal answer
-        if visit.count(0) == 0:
-            answer = max(answer, tmp)
-        else:
-            for i in range(L):
-                if visit[i] == 0:
-                    visit[i-1], visit[i], visit[i+1] = visit[i-1]+1, visit[i]+1, visit[i+1]+1
-                    dfs(tmp+sticker[i])
-                    visit[i-1], visit[i], visit[i+1] = visit[i-1]-1, visit[i]-1, visit[i+1]-1
-    visit[0], visit[1], visit[-1] = 1, 1, 1
-    dfs(sticker[0])
-    visit[0], visit[1], visit[-1] = 0, 0, 0
-
-    visit[0], visit[-2], visit[-1] = 1, 1, 1
-    dfs(sticker[-1])
-    visit[0], visit[-2], visit[-1] = 0, 0, 0
-
-    visit[0], visit[-1] = 1, 1
-    dfs(0)
-    visit[0], visit[-1] = 0, 0
+    back = [0]*L
+    back[1] = sticker[1]
+    for i in range(2, L):
+        back[i] = max(back[i-1], back[i-2]+sticker[i])
+    answer = max(max(front), max(back))
     return answer
 
-print(solution3([14, 6, 5, 11, 3, 9, 2, 10]))
-print(solution3([1, 3, 2, 5, 4]))
+print(solution([14, 6, 5, 11, 3, 9, 2, 10]))
+print(solution([1, 3, 2, 5, 4]))
 
-
-
-# 2.
+# 2. 맨 앞이 떼어진 경우와 아닌 경우로 구분
 def solution2(sticker):
     answer = 0
     L = len(sticker)
@@ -60,16 +46,16 @@ def solution2(sticker):
     dfs(sticker[-1])
     visit[0], visit[-2], visit[-1] = 0, 0, 0
 
-    visit[0], visit[-1] = 1, 1
-    dfs(0)
-    visit[0], visit[-1] = 0, 0
+    # visit[0], visit[-1] = 1, 1
+    # dfs(0)
+    # visit[0], visit[-1] = 0, 0
     return answer
 
 print(solution2([14, 6, 5, 11, 3, 9, 2, 10]))
 print(solution2([1, 3, 2, 5, 4]))
 
 # 1. 완전탐색 풀이
-def solution(sticker):
+def solution1(sticker):
     answer = 0
     L = len(sticker)
     visit = [0]*L
@@ -88,10 +74,9 @@ def solution(sticker):
                     if i == 0: visit[-1], visit[i], visit[i+1] = visit[-1]-1, visit[i]-1, visit[i+1]-1
                     elif i == L-1: visit[i-1], visit[i], visit[0] = visit[i-1]-1, visit[i]-1, visit[0]-1
                     else: visit[i-1], visit[i], visit[i+1] = visit[i-1]-1, visit[i]-1, visit[i+1]-1
-
     dfs(0)
     return answer
 
-print(solution([14, 6, 5, 11, 3, 9, 2, 10]))
-print(solution([1, 3, 2, 5, 4]))
+print(solution1([14, 6, 5, 11, 3, 9, 2, 10]))
+print(solution1([1, 3, 2, 5, 4]))
 
--- 1025/최고의 집합.py
+++ 1025/최고의 집합.py
@@ -3,30 +3,95 @@
 # 1. 각 원소의 합이 S가 되는 수의 집합
 # 2. 위 조건을 만족하면서 각 원소의 곱 이 최대가 되는 집합
 
-# 런타임 에러???
-# 집합의 원소의 개수 n과 모든 원소들의 합 s
+# 최적화
 def solution(n, s):
+    if n > s:
+        return [-1]
+    value, remain = s//n, s%n
+    answer = [value]*n
+    for i in range(remain):
+        answer[i] += 1
+    answer.sort()
+    return answer
+
+# 2. 굳이 절반으로 나누면서 붙일 필요가 없다.
+def solution2(n, s):
+    answer = []
+    if n%2 == 1:
+        tmp = s//3
+        flag = s%3
+        if not flag:
+            answer.append(tmp)
+            s -= tmp
+        if flag:
+            answer.append(tmp+1)
+            s -= tmp+1
+    while len(answer) != n:
+        tmp = s//2
+        flag = s%2
+        if not flag:
+            answer.append(tmp)
+            answer.append(tmp)
+        if flag:
+            answer.append(tmp+1)
+            answer.append(tmp)
+    if answer.count(0):
+        answer = [-1]
+    answer.sort()
+    return answer
+
+# 런타임 에러??? -> RecursionError n의 길이가 10만 이상이므로 dfs가 1000회이상 들감
+def dfs(SumCnt, MulCnt, tmp, n, s):
+    global answer, MaxMulCnt
+    if len(tmp) == n:
+        if SumCnt == s and MaxMulCnt < MulCnt:
+            MaxMulCnt = MulCnt
+            if answer:
+                while answer: answer.pop()
+            for t in tmp:
+                answer.append(t)
+    else:
+        for num in range(s):
+            tmp.append(num)
+            dfs(SumCnt+num, MulCnt*num, tmp, n, s)
+            tmp.pop()
+
+def solution1(n, s):
+    global answer, MaxMulCnt
     # 최고의 집합이 존재하지 않는 경우 -1을 채워서 return
     answer = []
-
-    maxMulCnt = 1
-    # 최고의 집합을 return
-    def dfs(SumCnt, MulCnt, tmp):
-        nonlocal answer
-        if len(tmp) == n:
-            if SumCnt == s and maxMulCnt < MulCnt:
-                answer = tmp
-                print(tmp)
-        else:
-            for num in range(s):
-                tmp.append(num)
-                dfs(SumCnt+num, MulCnt*num, tmp)
-                tmp.pop()
-
-    dfs(0, 1, [])
-
+    MaxMulCnt = 1
+    # 최고의 집합을 오름차순으로 정렬된 1차원 배열(list, vector) return
+    dfs(0, 1, [], n, s)
+    if answer == []: answer.append(-1)
+    answer.sort()
     return answer
+
+
+# 처음 짠 dfs
+# 집합의 원소의 개수 n과 모든 원소들의 합 s
+# def solution1(n, s):
+#     # 최고의 집합이 존재하지 않는 경우 -1을 채워서 return
+#     answer = []
+#     maxMulCnt = 1
+#     # 최고의 집합을 return
+#     def dfs(SumCnt, MulCnt, tmp):
+#         nonlocal answer
+#         if len(tmp) == n:
+#             if SumCnt == s and maxMulCnt < MulCnt:
+#                 answer = tmp
+#                 print(tmp)
+#         else:
+#             for num in range(s):
+#                 tmp.append(num)
+#                 dfs(SumCnt+num, MulCnt*num, tmp)
+#                 tmp.pop()
+#     dfs(0, 1, [])
+#     return answer
 
 print(solution(2, 9)) # [4, 5]
 print(solution(2, 1)) # [-1]
 print(solution(2, 8)) # [4, 4]
+print(solution(3, 12))
+print(solution(3, 13))
+# print(solution(10000, 100000))
