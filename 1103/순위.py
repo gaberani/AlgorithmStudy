00a46a1e39a3dfcc3c778327d0064837701f88cd
gaberani <khs0783@naver.com>
2020-11-03 16:31:00 +0900

    프로그래머스 순위 완료

--- 1103/순위.py
+++ 1103/순위.py
@@ -1,5 +1,5 @@
 # n명의 권투선수가 권투 대회 각각 1번부터 n번까지 번호를 받았습니다.
-# 만약 A 선수가 B 선수보다 실력이 좋다면 A는 B를 항상 이깁니다.
+# 만약 A 선수가 B 선수보다 실력이 좋다면 A는 B를 항상 이깁니다. => 실력순이다.
 # 심판은 주어진 경기 결과를 가지고 선수들의 순위를 매기려 합니다.
 
 # 선수의 수 n
@@ -7,16 +7,30 @@
 # 정확하게 순위를 매길 수 있는 선수의 수를 return
 def solution(n, results):
     answer = 0
-    L = len(results)
-    grid = [[0]*L for _ in range(L)]
-    for r1, r2 in results:
-        grid[r1][r2] = -1
-        grid[r2][r1] = 1
-    for i in range(L):
-        for j in range(L):
-            if grid[i][L]:
+    winners = {i:set() for i in range(1, n+1)}
+    losers = {i:set() for i in range(1, n+1)}
 
-            if grid[L][i]:
+    for r1, r2 in results:
+        winners[r1].add(r2)
+        losers[r2].add(r1)
+    # print(winners)
+    # print(losers)
+    # print('----------')
+    for i in range(1, n+1):
+        # i에게 진 사람들은 i를 이긴 사람에게 짐
+        for loser in winners[i]:
+            losers[loser] |= losers[i]
+
+        # i를 이긴 사람들은 i에게 진 사람도 이김
+        for winner in losers[i]:
+            winners[winner] |= winners[i]
+
+    # print(winners)
+    # print(losers)
+    for i in range(1, n+1):
+        if len(winners[i]) + len(losers[i]) == n-1:
+            answer += 1
+
     return answer
 
-print(solution([[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]], 2))
+print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
