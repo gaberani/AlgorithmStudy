85fe2d6064ec28f40e1b962b11ae28d80fbdedc8
gaberani <khs0783@naver.com>
2020-11-08 16:19:10 +0900

    백준 20056 마법사 상어와 파이어볼 완료

--- 1108/마법사 상어와 파이어볼.py
+++ 1108/마법사 상어와 파이어볼.py
@@ -8,12 +8,10 @@ dc = [0, 1, 1, 1, 0, -1, -1, -1]
 
 N, M, K = map(int, input().split())
 info_lsts = [list(map(int, input().split())) for _ in range(M)]
-# grid = [[0]*N for _ in range(N)]
 fire_info = {}
 # i번 파이어볼의 위치는 (ri, ci), 질량은 mi이고, 방향은 di, 속력은 si이다.
 for ri, ci, mi, si, di in info_lsts:
     fire_info[(ri-1, ci-1)] = [(mi, si, di)]
-# 마법사 상어가 모든 파이어볼에게 이동을 명령하면 다음이 일들이 일어난다.
 
 # 1. 모든 파이어볼이 자신의 방향 di로 속력 si칸 만큼 이동한다.
 #    이동하는 중에는 같은 칸에 여러 개의 파이어볼이 있을 수도 있다.
@@ -25,7 +23,6 @@ def move():
         for val in values:
             fm, fs, fd = val
             tmp.append([(fi+fs*dr[fd])%N, (fj+fs*dc[fd])%N, fm, fs, fd])
-    # print(fire_info)
 
     fire_info = {}
     for ni, nj, nm, ns, nd in tmp:
@@ -34,22 +31,16 @@ def move():
         else:
             fire_info[(ni, nj)] = [(nm, ns, nd)]
 
-# 2. 이동이 모두 끝난 뒤, 2개 이상의 파이어볼이 있는 칸에서는 다음과 같은 일이 일어난다.
-#   ㄱ. 같은 칸에 있는 파이어볼은 모두 하나로 합쳐진다.
-#   ㄴ. 파이어볼은 4개의 파이어볼로 나누어진다.
-#   ㄷ. 나누어진 파이어볼의 질량, 속력, 방향은 다음과 같다.
-#       가. 질량은 ⌊(합쳐진 파이어볼 질량의 합)/5⌋이다.
-#       나. 속력은 ⌊(합쳐진 파이어볼 속력의 합)/(합쳐진 파이어볼의 개수)⌋이다.
-#       다. 합쳐지는 파이어볼의 방향이 모두 홀수이거나 모두 짝수이면,
-#           방향은 0, 2, 4, 6이 되고, 그렇지 않으면 1, 3, 5, 7이 된다.
-#   ㄹ. 질량이 0인 파이어볼은 소멸되어 없어진다.
 def multifireball():
     global fire_info
     tmp = []
     for key, values in fire_info.items():
         balls_m, balls_s, balls_d = 0, 0, -1
         fi, fj = key
+        # 2. 이동이 모두 끝난 뒤, 2개 이상의 파이어볼이 있는 칸에서는 다음과 같은 일이 일어난다.
         if len(values) > 1:
+            # ㄱ. 같은 칸에 있는 파이어볼은 모두 하나로 합쳐진다.
+
             for val in values:
                 fm, fs, fd = val
                 if balls_d == -1:
@@ -61,9 +52,17 @@ def multifireball():
 
                 balls_m += fm
                 balls_s += fs
+
+            #   ㄷ. 나누어진 파이어볼의 질량, 속력, 방향은 다음과 같다.
+            #       가. 질량은 ⌊(합쳐진 파이어볼 질량의 합)/5⌋이다.
+            #       나. 속력은 ⌊(합쳐진 파이어볼 속력의 합)/(합쳐진 파이어볼의 개수)⌋이다.
             nm, ns = balls_m//5, balls_s//len(values)
+            #       다. 합쳐지는 파이어볼의 방향이 모두 홀수이거나 모두 짝수이면,
+            #           방향은 0, 2, 4, 6이 되고, 그렇지 않으면 1, 3, 5, 7이 된다.
             nd = [0, 2, 4, 6] if balls_d != 7 else [1, 3, 5, 7]
+            # ㄹ. 질량이 0인 파이어볼은 소멸되어 없어진다.
             if nm > 0:
+                # ㄴ. 파이어볼은 4개의 파이어볼로 나누어진다.
                 for d in nd:
                     tmp.append([fi, fj, nm, ns, d])
         else:
