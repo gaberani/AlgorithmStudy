a55657a60be1fc1b290ed9aa4b808c99c9b77043
gaberani <khs0783@naver.com>
2020-11-18 23:34:08 +0900

    Leet 01 Matrix

---
+++ 1119/01 Matrix.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/01-matrix/
+
+# Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
+# The distance between two adjacent cells is 1.
+
+# The number of elements of the given matrix will not exceed 10,000.
+# There are at least one 0 in the given matrix.
+# The cells are adjacent in only four directions: up, down, left and right.
+
+
+class Solution:
+    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        pass
