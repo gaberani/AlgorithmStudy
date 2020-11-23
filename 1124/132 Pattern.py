8235edbf62e9b09729cfc11afd367bc05b0c5c08
gaberani <khs0783@naver.com>
2020-11-23 23:30:16 +0900

    Leet 132 Pattern 예제 등록

--- 1124/132 Pattern.py
+++ 1124/132 Pattern.py
@@ -1,2 +1,11 @@
+# Input: nums = [1,2,3,4]
+# Output: false
+
+# Input: nums = [3,1,4,2]
+# Output: true
+
+# Input: nums = [-1,3,2,0]
+# Output: true
+
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
