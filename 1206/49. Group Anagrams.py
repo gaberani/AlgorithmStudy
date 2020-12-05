4bb3fa024e0667bd7fd1d46c4bcd1d90171d9521
gaberani <khs0783@naver.com>
2020-12-05 16:29:44 +0900

    Leet 49. Group Anagrams 완ㄹ

--- 1206/49. Group Anagrams.py
+++ 1206/49. Group Anagrams.py
@@ -4,6 +4,23 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
+        str_dic = []
+        for str in strs:
+            s_dic = {}
+            # 한글자씩 따기
+            for s in str:
+                s_dic[s] = 1 if s_dic.get(s) == None else s_dic[s] + 1
+
+            if str_dic:
+                # print(s_dic, [k[0] for k in str_dic])
+                if s_dic not in [k[0] for k in str_dic]:
+                    str_dic.append([s_dic, str])
+                else:
+                    str_dic[[k[0] for k in str_dic].index(s_dic)].append(str)
+            else:
+                str_dic.append([s_dic, str])
+        return [a[1:] for a in str_dic]
+
 
 test = Solution()
 print(test.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
