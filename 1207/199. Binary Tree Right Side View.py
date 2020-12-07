4bbdabca6e6e534d5ae9a2ff43c34f57caa243a0
gaberani <khs0783@naver.com>
2020-12-07 18:46:13 +0900

    Leet 199. Binary Tree Right Side View 완료

--- 1207/199. Binary Tree Right Side View.py
+++ 1207/199. Binary Tree Right Side View.py
@@ -8,12 +8,31 @@
 # Given a binary tree, imagine yourself standing on the right side of it,
 # return the values of the nodes you can see ordered from top to bottom.
 class Solution(object):
-    def rightSideView(self, root):
+    def rightSideView2(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
+        answer = [root.val]
+        while root.right == None:
 
+        if not answer:
+            answer.append(r.val)
+        else:
+            answer.append(r.right)
+
+    def rightSideView(self, root):
+        # 비어있을 경우
+        if root == None:
+            return []
+        # 초기값
+        ans = [root.val]
+        # DFS로 반복해서 left, right 각각 붙여내기
+        left = ans + self.rightSideView(root.left)
+        right = ans + self.rightSideView(root.right)
+        if len(right) >= len(left):
+            return right
+        return right + left[len(right):]
 
 # Example
 # Input: [1,2,3,null,5,null,4]
@@ -25,3 +44,8 @@ class Solution(object):
 # 2     3         <---
 #  \     \
 #   5     4       <---
+
+# [1, 2, 3, 4]
+# TreeNode{val: 1,
+#          left: TreeNode{val: 2, left: TreeNode{val: 4, left: None, right: None}, right: None},
+#          right: TreeNode{val: 3, left: None, right: None}}
