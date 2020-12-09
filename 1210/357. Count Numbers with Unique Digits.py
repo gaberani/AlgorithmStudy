797de45d9362c69632d810b0189d857531baf847
gaberani <khs0783@naver.com>
2020-12-09 21:40:32 +0900

    Leet 357. Count Numbers with Unique Digits 푸는 중

--- 1210/357. Count Numbers with Unique Digits.py
+++ 1210/357. Count Numbers with Unique Digits.py
@@ -8,11 +8,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-
         # n과 같은 길이를 갖고 있고 
         # set를 써서 중복없애면 길이가 1인 경우 전부 제외
+        for i in range(10**n):
+            print(i)
+            
         
 # Input: 2
 # Output: 91 
 # Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, 
 #              excluding 11,22,33,44,55,66,77,88,99
+
+test = Solution()
+print(test.countNumbersWithUniqueDigits(2))
+# print(test.countNumbersWithUniqueDigits(0))
