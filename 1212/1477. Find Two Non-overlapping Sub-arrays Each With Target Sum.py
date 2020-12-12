d88db57e48adcca691d2a08c2be17a0ec215be78
gaberani <khs0783@naver.com>
2020-12-12 15:48:51 +0900

    Leet 1477  푸는중

--- 1212/1477. Find Two Non-overlapping Sub-arrays Each With Target Sum.py
+++ 1212/1477. Find Two Non-overlapping Sub-arrays Each With Target Sum.py
@@ -1,3 +1,5 @@
+# 슬라이딩 윈도우 알고리즘
+
 # two non-overlapping sub-arrays
 # you have to find an answer where the sum of the lengths of the two sub-arrays is minimum.
 
@@ -11,9 +13,37 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        #
+        L = len(arr)
+        if L == 1:
+            return -1
+
+        subarrays = []
+        sub = []
+        for i in range(L):
+            sum_result = sum(sub) + arr[i]
+            if sum_result == target:
+                subarrays += ([sub+[arr[i]]])
+                sub = []
+            elif sum_result > target:
+                if arr[i] == target:
+                    sub = []
+                    subarrays.append([arr[i]])
+                else:
+                    sub = [arr[i]]
+            elif sum_result < target:
+                sub.append(arr[i])
+            # print(sub, arr[i], subarrays)
+        if sum(sub) == target:
+            subarrays.append(sub)
+        print(subarrays)
+
+        if len(subarrays) < 2:
+            return -1
+        subarrays = sorted(subarrays, key= lambda x: len(x))
+        return len(subarrays[0])+len(subarrays[1])
 
 test = Solution()
-print(test.minSumOfLengths([3, 2, 2, 4, 3])) # 3
-print(test.minSumOfLengths([7, 3, 4, 7])) # 7
-print(test.minSumOfLengths([4, 3, 2, 6, 2, 3, 4])) # 6
+print(test.minSumOfLengths([3, 2, 2, 4, 3], 3)) # 2
+print(test.minSumOfLengths([7, 3, 4, 7], 7)) # 2
+print(test.minSumOfLengths([4, 3, 2, 6, 2, 3, 4], 6)) # -1
+print(test.minSumOfLengths([1, 2, 2, 3, 2, 6, 7, 2, 1, 4, 8], 5)) # -1
