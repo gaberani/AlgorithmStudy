8c5efbd1e32a9e295f6513f80f016bd1d3ceb963
gaberani <khs0783@naver.com>
2020-12-20 21:54:16 +0900

    Leet 120 푸는중

--- 1220/120. Triangle.py
+++ 1220/120. Triangle.py
@@ -13,6 +13,7 @@
 
 class Solution(object):
     def minimumTotal(self, triangle):
+        global answer
         """
         :type triangle: List[List[int]]
         :rtype: int
@@ -20,3 +21,51 @@ class Solution(object):
         # 각 스텝마다 최소를 더한 path를 구하기
         # dfs
         # 백트래킹없이 완전 탐색
+
+        def dfs(step, cnt, preIdx):
+            global answer
+            if step == L:
+                answer = min(answer, cnt)
+            else:
+                for i in range(preIdx, preIdx+2):
+                    if 0 <= i < step+1:
+                        dfs(step+1, cnt+triangle[step][i], i)
+
+        L = len(triangle)
+        answer = 2**31
+        dfs(0, 0, 0)
+        return answer
+
+    def minimumTotal2(self, triangle):
+        global answer
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        # 각 스텝마다 최소를 더한 path를 구하기
+        # dfs
+        # 백트래킹없이 완전 탐색
+
+        def dfs(step, numList):
+            global answer
+            if step == L:
+                print(numList)
+                for numInfo in numList:
+                    answer = min(answer, numInfo[0])
+            else:
+                tmp = []
+                for numInfo in numList:
+                    num, preIdx = numInfo
+                    for i in range(preIdx, preIdx+2):
+                        if 0 <= i < step+1:
+                            tmp.append([num+triangle[step][i], i])
+                dfs(step+1, tmp)
+
+        L = len(triangle)
+        answer = 2**31
+        dfs(1, [[triangle[0][0], 0]])
+        return answer
+
+test = Solution()
+print(test.minimumTotal2([[2],[3,4],[6,5,7],[4,1,8,3]]))
+print(test.minimumTotal2([[-1],[3,2],[-3,1,-1]]))
